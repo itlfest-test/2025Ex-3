e66d067d6456d2599597b98f0f2eeba31a3dd277
iTLFesttester <koki.gatos12@gmail.com>
2026-04-12 03:28:28 +0900

    Create events.js

---
+++ events.js
@@ -0,0 +1,98 @@
+[
+  {
+    "id": 101,
+    "name": "お笑いライブ2025",
+    "subtitle": "",
+    "description": "人気芸人3組がiTLに集結！事前チケットを入手して一緒に楽しみましょう！",
+    "university": "中央（市谷田町）",
+    "category": "エンタメ・お笑い",
+    "field": "その他",
+    "startDatetime": "2025-11-01T13:00:00",
+    "endDatetime": "2025-11-01T14:00:00",
+    "location": "3F301"
+  },
+  {
+    "id": 102,
+    "name": "田﨑史郎先生講演会",
+    "subtitle": "",
+    "description": "メディアへの露出も多い中大OB、田﨑先生が登壇！これからの社会に有用な情報を多く得られます。",
+    "university": "中央（市谷田町）",
+    "category": "講演・セミナー",
+    "field": "社会",
+    "startDatetime": "2025-11-02T14:00:00",
+    "endDatetime": "2025-11-02T15:00:00",
+    "location": "3F301"
+  },
+  {
+    "id": 103,
+    "name": "コスプレダンス",
+    "subtitle": "",
+    "description": "コスプレをしたメンバーが、ボカロ・アニソンと共に踊ります！あなたの好きなキャラクターと、写真が撮れちゃうかも…！？ぜひ見に来てください！",
+    "university": "中央（市谷田町）",
+    "category": "音楽・パフォーマンス",
+    "field": "文化",
+    "startDatetime": "2025-11-01T13:00:00",
+    "endDatetime": "2025-11-01T14:00:00",
+    "location": "9F901"
+  },
+  {
+    "id": 104,
+    "name": "iTLピアノサークル",
+    "subtitle": "",
+    "description": "iTLピアノサークルでは、昨年度に引き続きクラシック音楽の演奏を行います！ピアノやヴァイオリンの優雅な音色をお楽しみください♪",
+    "university": "中央（市谷田町）",
+    "category": "音楽・パフォーマンス",
+    "field": "文化",
+    "startDatetime": "2025-11-01T14:30:00",
+    "endDatetime": "2025-11-01T15:30:00",
+    "location": "1Fステージ"
+  },
+  {
+    "id": 105,
+    "name": "自主制作アニメーション上映",
+    "subtitle": "",
+    "description": "会員制作の自主制作アニメーションを上映！今年度は新たにロボットアニメを上映予定！",
+    "university": "中央（市谷田町）",
+    "category": "展示・発表",
+    "field": "芸術",
+    "startDatetime": "2025-11-01T16:00:00",
+    "endDatetime": "2025-11-01T17:00:00",
+    "location": "1Fモニター"
+  },
+  {
+    "id": 106,
+    "name": "iTL MUSIC FESTiVAL 2025",
+    "subtitle": "",
+    "description": "iTL唯一の軽音サークルによるライブです。ポップスからロックまで多彩な音楽をお届けします。きっと推しバンドが見つかるはずです！ぜひお楽しみください！",
+    "university": "中央（市谷田町）",
+    "category": "音楽・パフォーマンス",
+    "field": "芸術",
+    "startDatetime": "2025-11-02T10:00:00",
+    "endDatetime": "2025-11-02T17:00:00",
+    "location": "1Fステージ"
+  },
+  {
+    "id": 107,
+    "name": "C3",
+    "subtitle": "PlayLAB",
+    "description": "展示のテーマは技術×表現。AIを使ったポストカードや音楽の制作体験に、空中に映像を映すホログラム等が勢揃い。制作物は持ち帰れます。思い出作りにぜひ。",
+    "university": "中央（市谷田町）",
+    "category": "展示・発表",
+    "field": "理工",
+    "startDatetime": "2025-11-01T10:00:00",
+    "endDatetime": "2025-11-01T17:00:00",
+    "location": "7F701"
+  },
+  {
+    "id": 108,
+    "name": "C3",
+    "subtitle": "AIポストカード生成",
+    "description": "展示のテーマは技術×表現。AIを使ったポストカードや音楽の制作体験に、空中に映像を映すホログラム等が勢揃い。制作物は持ち帰れます。思い出作りにぜひ。",
+    "university": "中央（市谷田町）",
+    "category": "展示・発表",
+    "field": "理工",
+    "startDatetime": "2025-11-02T10:00:00",
+    "endDatetime": "2025-11-02T17:00:00",
+    "location": "7F701"
+  }
+]
